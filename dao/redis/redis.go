a90f3aeebb24c139bfb25349442c0f0c97f3ec57
wangxin <wang.xin@bytedance.com>
2022-04-05 23:32:38 +0800

    fix: init redis client

--- dao/redis/redis.go
+++ dao/redis/redis.go
@@ -7,16 +7,16 @@ import (
 
 var Client *redis.Client
 
-func initRedisClient() {
+func InitRedisClient() {
 	Client = redis.NewClient(&redis.Options{
 		Addr:     "carguru.ow3jum.0001.use1.cache.amazonaws.com:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
 
-	if _, err := Client.Ping().Result(); err != nil {
-		panic(err)
-	}
+	//if _, err := Client.Ping().Result(); err != nil {
+	//	panic(err)
+	//}
 }
 
 func GenCarKey(listID string) string {
